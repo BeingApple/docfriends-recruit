83ca8edd0d8ce26648485c1d06518e3fd3fa5d77
yong <yong@goand.com>
2020-09-12 23:00:35 +0900

    결과물 출력 구문 추가

--- PS/src/ProblemSolving.java
+++ PS/src/ProblemSolving.java
@@ -1,3 +1,4 @@
+import util.IndexFinder;
 import util.NumberExtractor;
 
 import java.util.List;
@@ -6,5 +7,10 @@ public class ProblemSolving {
     public static void main(String[] args) {
         NumberExtractor numberExtractor = new NumberExtractor("ab23c4d56e78f9g12h34i5j12k45l67n89m99o1k123p456q567r768s890t67u456v345w234x23y239z");
         List<Integer> integers = numberExtractor.getNumbers();
+
+        IndexFinder indexFinder = new IndexFinder(integers);
+        System.out.println("maxIndex : "+indexFinder.getMaxIndex());
+        System.out.println("minIndex : "+indexFinder.getMinIndex());
+        System.out.println("addIndexes : "+indexFinder.addIndex());
     }
 }
